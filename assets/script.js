e376917b3eaa980b60a72fb6653667db1e0a929d
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-06-09 21:35:51 +1000

    new version using jquery

--- assets/script.js
+++ assets/script.js
@@ -1,94 +1,72 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-let shuffledQuestions, currentQuestionIndex
-const questionElement = document.getElementById("question")
-const answerButtons = document.getElementById("answer-buttons")
+// variables
+const $startBtn = $("#start-btn");
+const $nextBtn = $("#next-btn");
+const $startText = $("#start-text");
+const $contQuestion = $("#question-container");
+const $question = $("#question");
+const $answerList = $("#answer-buttons");
 
-
-startButton.addEventListener("click", startGame);
+$startBtn.on("click", startGame);
 
 function startGame() {
-    startButton.classList.add("hide");
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetState();
+    $startText.addClass("hide");
+    $contQuestion.removeClass("hide");
     showQuestion();
 }
 
 function showQuestion() {
-    shuffledQuestions = questions[Math.floor(Math.random() * questions.length)];
+    const randomQuestion = quizQuestions[Math.floor(Math.random() * quizQuestions.length)];
 
-    questionElement.innerText = shuffledQuestions.question;
-    shuffledQuestions.answers.forach(answer => {
-        const button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtons.appendChild(button);
-    })
+    $question.append(randomQuestion.question);
+    randomQuestion.answers.forEach(element => {
+
+
+    });
 }
 
-function resetState() {
-    nextButton.classList.add("hide");
-    // while (answerButtons.childElementCount > 0) {
-    //     answerButtons.removeChild();
-    // }
 
-}
-
-function selectAnswer(e) {
-
-}
-
-const questions = [{
-        question: "What is the HTML tag under which one can write the JavaScript code?",
-        answers: [
-            { text: "<javascript>", correct: false },
-            { text: "<scripted>", correct: false },
-            { text: "<script>", correct: true },
-            { text: "<js>", correct: false }
-        ]
+//questions for the quiz
+const quizQuestions = [{
+    question: "What is the HTML tag under which one can write the JavaScript code?",
+    answers: {
+        A: "<javascript>",
+        B: "<scripted>",
+        C: "<script>",
+        D: "<js>",
     },
-    {
-        question: "Which of the following is the correct syntax to display “Hello World!” in an alert box using JavaScript?",
-        answers: [
-            { text: "alertbox('Hello World!');", correct: false },
-            { text: "msg('Hello World!');", correct: false },
-            { text: "msgbox('Hello World!');", correct: false },
-            { text: "alert('Hello World!');", correct: true }
-        ]
+    correctAnswer: "C"
+}, {
+    question: "Which of the following is the correct syntax to display “Hello World!” in an alert box using JavaScript?",
+    answers: {
+        A: "alertbox('Hello World!');",
+        B: "msg('Hello World!');",
+        C: "msgbox('Hello World!');",
+        D: "alert('Hello World!');",
     },
-    {
-        question: "The external JavaScript file must contain <script> tag. True or False?",
-        answers: [
-            { text: "True", correct: false },
-            { text: "False", correct: true }
-        ]
+    correctAnswer: "D"
+}, {
+    question: "The external JavaScript file must contain <script> tag. True or False?",
+    answers: {
+        A: "True",
+        B: "False",
     },
-    {
-        question: "Which of the following is not a reserved word in JavaScript?",
-        answers: [
-            { text: "interface", correct: false },
-            { text: "throws", correct: false },
-            { text: "program", correct: true },
-            { text: "short", correct: false }
-        ]
+    correctAnswer: "B"
+}, {
+    question: "Which of the following is not a reserved word in JavaScript?",
+    answers: {
+        A: "interface",
+        B: "throws",
+        C: "program",
+        D: "short",
     },
-    {
-        question: "In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?",
-        answers: [
-            { text: "isInteger(value)", correct: true },
-            { text: "ifInteger(value)", correct: false },
-            { text: "Integer(value)", correct: false },
-            { text: "ifinteger(value)", correct: false }
-        ]
-    }
-];
+    correctAnswer: "C"
+}, {
+    question: "In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?",
+    answers: {
+        A: "isInteger(value)",
+        B: "ifInteger(value)",
+        C: "Integer(value)",
+        D: "ifinteger(value)",
+    },
+    correctAnswer: "A"
+}]
